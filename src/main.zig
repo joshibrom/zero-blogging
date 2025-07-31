f29ca05c0aefb04b6e727bd46edd6e9f3fd9b261
Joshua Ibrom <joshuaibrom@gmail.com>
2025-07-30 22:01:47 -0500

    feat(templates): read template file.

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,14 @@
 //! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
+const TEMPL_BASE = @embedFile("content/templates/base.must.html");
+
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Hello, world!\n", .{});
+    try stdout.print("{s}\n", .{TEMPL_BASE});
 
     try bw.flush(); // Don't forget to flush!
 }
