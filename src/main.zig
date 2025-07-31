8b5fa82238cb8511cc41b2b66d58b8de3eaee09b
Joshua Ibrom <joshuaibrom@gmail.com>
2025-07-31 18:08:57 -0500

    refactor(templates): move templates to views dir outside src

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,23 @@
 //! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
-const TEMPL_BASE = @embedFile("content/templates/base.must.html");
+const Allocator = std.mem.Allocator;
+
+const TEMPL_BASE = "views/base.must.html";
+
+fn readToString(allocator: Allocator, fname: []const u8) ![]const u8 {
+    const f = try std.fs.cwd().openFile(fname, .{});
+    defer f.close();
+    return f.readToEndAlloc(allocator, 10000);
+}
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("{s}\n", .{TEMPL_BASE});
+    const templ = try readToString(std.heap.page_allocator, TEMPL_BASE);
+    try stdout.print("{s}\n", .{templ});
 
     try bw.flush(); // Don't forget to flush!
 }
