9cb8b25c523dae9d26f15ffde077414ce11c6e15
Joshua Ibrom <joshuaibrom@gmail.com>
2025-07-30 21:18:58 -0500

    First commit. Init repo.

---
+++ src/main.zig
@@ -0,0 +1,14 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Hello, world!\n", .{});
+
+    try bw.flush(); // Don't forget to flush!
+}
